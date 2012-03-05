a6bb1c18f162d29a360afbb9ea90da7d148d16b2
nguyendung.dce.hut <nguyendung.dce.hut@gmail.com>
2012-03-05 08:09:53 +0000

    sửa được một số lỗi và đã chạy khá ổn định,chưa test trên thiết bị thật..Chưa có cơ chế xử lý lỗi và xử lý đầu vào

--- VietnameseTTSMini2440/src/ktmt/k52/viettts/HttpHelp.java
+++ VietnameseTTSMini2440/src/ktmt/k52/viettts/HttpHelp.java
@@ -1,6 +1,10 @@
 package ktmt.k52.viettts;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -12,143 +16,227 @@ import org.apache.http.client.params.CookiePolicy;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EncodingUtils;
 import org.apache.http.util.EntityUtils;
+
+import android.util.Xml.Encoding;
 
 public class HttpHelp {
 
-    static DefaultHttpClient httpClient = new DefaultHttpClient();
-    static HttpContext localContext = new BasicHttpContext();
-    private static boolean abort;
-    private static String ret;
+	DefaultHttpClient httpClient;
+	HttpContext localContext;
+	private boolean abort;
+	private String ret;
 
-    static HttpResponse response = null;
-    static HttpPost httpPost = null;
+	HttpResponse response = null;
+	HttpPost httpPost = null;
 
-    public HttpHelp(){
+	public HttpHelp() {
+		httpClient = new DefaultHttpClient();
+		localContext = new BasicHttpContext();
+	}
 
-    }
+	public void clearCookies() {
 
-    public static void clearCookies() {
+		httpClient.getCookieStore().clear();
 
-    	httpClient.getCookieStore().clear();
+	}
 
-    }
+	public void abort() {
 
-    public static void abort() {
+		try {
+			if (httpClient != null) {
+				System.out.println("Abort.");
+				httpPost.abort();
+				abort = true;
+			}
+		} catch (Exception e) {
+			System.out.println("HTTPHelp : Abort Exception : " + e);
+		}
+	}
 
-    	try {
-    		if(httpClient!=null){
-    			System.out.println("Abort.");
-    			httpPost.abort();
-    			abort = true;
-    		}
-    	} catch (Exception e) {
-    		System.out.println("HTTPHelp : Abort Exception : "+e);
-    	}
-    }
+	public String postPage(String url, String data, boolean returnAddr)
+			throws ParseException, IOException {
 
-    public static String postPage(String url, String data, boolean returnAddr) throws ParseException, IOException 
-    {
+		ret = null;
 
-    	ret = null;
+		httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY,
+				CookiePolicy.RFC_2109);
 
-    	httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.RFC_2109);
+		httpPost = new HttpPost(url);
+		response = null;
 
-    	httpPost = new HttpPost(url);
-    	response = null;
+		StringEntity tmp = null;
 
-    	StringEntity tmp = null;		
+		httpPost.setHeader(
+				"User-Agent",
+				"Mozilla/5.0 (X11; U; Linux "
+						+ "i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)");
+		httpPost.setHeader(
+				"Accept",
+				"text/html,application/xml,"
+						+ "application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5");
+		httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    	httpPost.setHeader("User-Agent", "Mozilla/5.0 (X11; U; Linux " +
-    		"i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)");
-    	httpPost.setHeader("Accept", "text/html,application/xml," +
-    		"application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5");
-    	httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+		try {
+			tmp = new StringEntity(data, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			System.out
+					.println("HTTPHelp : UnsupportedEncodingException : " + e);
+		}
 
-    	try {
-    		tmp = new StringEntity(data,"UTF-8");
-    	} catch (UnsupportedEncodingException e) {
-    		System.out.println("HTTPHelp : UnsupportedEncodingException : "+e);
-    	}
+		httpPost.setEntity(tmp);
 
-    	httpPost.setEntity(tmp);
+		try {
+			response = httpClient.execute(httpPost, localContext);
+		} catch (ClientProtocolException e) {
+			System.out.println("HTTPHelp : ClientProtocolException : " + e);
+		} catch (IOException e) {
+			System.out.println("HTTPHelp : IOException : " + e);
+		}
+		ret = response.getStatusLine().toString();
+		HttpEntity entity = response.getEntity();
+		String responseText = EntityUtils.toString(entity);
 
-    	try {
-    		response = httpClient.execute(httpPost,localContext);
-    	} catch (ClientProtocolException e) {
-    		System.out.println("HTTPHelp : ClientProtocolException : "+e);
-    	} catch (IOException e) {
-    		System.out.println("HTTPHelp : IOException : "+e);
-    	} 
-                ret = response.getStatusLine().toString();
-                HttpEntity entity = response.getEntity();
-                String responseText = EntityUtils.toString(entity);
+		return responseText;
+	}
 
-                return responseText;
-                }
-    
-    public static String postPageIsolar(String data) throws ParseException, IOException
-    {
-    	//giong het phuong thuc tren nhung danh rieng cho trang isolar.vn
-    	
-    	data = "voice=male1&SSinput="+data+"&formSubmit=Submit";
+	public String postPageIsolar(String data) throws ParseException,
+			IOException, URISyntaxException {
+		// giong het phuong thuc tren nhung danh rieng cho trang isolar.vn
+
+		data = "voice=male1&SSinput=" + data + "&formSubmit=Submit";
+
 		String url = "http://isolar.vn/social/demo.php";
-    	ret = null;
+		ret = null;
 
-    	httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.RFC_2109);
+		httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY,
+				CookiePolicy.RFC_2109);
 
-    	httpPost = new HttpPost(url);
-    	response = null;
+		httpPost = new HttpPost(url);
+		response = null;
 
-    	StringEntity tmp = null;		
+		StringEntity tmp = null;
 
-    	httpPost.setHeader("User-Agent", "Mozilla/5.0 (X11; U; Linux " +
-    		"i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)");
-    	httpPost.setHeader("Accept", "text/html,application/xml," +
-    		"application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5");
-    	httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+		httpPost.setHeader(
+				"User-Agent",
+				"Mozilla/5.0 (X11; U; Linux "
+						+ "i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)");
+		httpPost.setHeader("Accept",
+				"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 
-    	try {
-    		tmp = new StringEntity(data,"UTF-8");
-    	} catch (UnsupportedEncodingException e) {
-    		System.out.println("HTTPHelp : UnsupportedEncodingException : "+e);
-    	}
+		httpPost.setHeader("Host", "isolar.vn");
+		httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    	httpPost.setEntity(tmp);
+		try {
+			tmp = new StringEntity(data, HTTP.UTF_8);
+		} catch (UnsupportedEncodingException e) {
+			System.out
+					.println("HTTPHelp : UnsupportedEncodingException : " + e);
+		}
 
-    	try {
-    		response = httpClient.execute(httpPost,localContext);
-    	} catch (ClientProtocolException e) {
-    		System.out.println("HTTPHelp : ClientProtocolException : "+e);
-    	} catch (IOException e) {
-    		System.out.println("HTTPHelp : IOException : "+e);
-    	} 
-                ret = response.getStatusLine().toString();
-                HttpEntity entity = response.getEntity();
-                String responseText = EntityUtils.toString(entity);
+		httpPost.setEntity(tmp);
 
-                return responseText;
-    }
-    
-    public static String getIsolarAudioUrl(String response)
-    {
-    	try
-    	{
-    	// xu ly reponse string
-		int test = response.indexOf("href");
-		int test2 = response.lastIndexOf("title");
-		String test3 = response.substring(test + 6, test2-2);
+		try {
+			response = httpClient.execute(httpPost, localContext);
+		} catch (ClientProtocolException e) {
+			System.out.println("HTTPHelp : ClientProtocolException : " + e);
+		} catch (IOException e) {
+			System.out.println("HTTPHelp : IOException : " + e);
+		}
+		ret = response.getStatusLine().toString();
+		HttpEntity entity = response.getEntity();
+		String responseText = EntityUtils.toString(entity);
 
-		String urlAudio = "http://isolar.vn/social/" + test3;
-		return urlAudio;
-    	}catch(Exception ex)
-    	{
-    		System.out.println(ex.getMessage());
-    	}
+		return responseText;
+	}
+
+	public String getIsolarAudioUrl(String response) {
+		try {
+			// xu ly reponse string
+			int test = response.indexOf("href");
+			int test2 = response.lastIndexOf("title");
+			String test3 = response.substring(test + 6, test2 - 2);
+
+			String urlAudio = "http://isolar.vn/social/" + test3;
+			return urlAudio;
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 		return null;
-    }
+	}
+
+	public String postPageVozMe(String data) throws ParseException, IOException {
+		// giong het phuong thuc tren nhung danh rieng cho trang
+		// http://vozme.com
+		// url = host+POST
+
+		String newdata = "interface=full&gn=ml&text="
+				+ data.replaceAll(" ", "+");
+		newdata = newdata.replaceAll("\000", "");
+
+		String url = "http://vozme.com/text2voice.php?lang=en";
+		ret = null;
+
+		httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY,
+				CookiePolicy.RFC_2109);
+
+		httpPost = new HttpPost(url);
+		response = null;
+
+		StringEntity tmp = null;
+
+		httpPost.setHeader(
+				"User-Agent",
+				"Mozilla/5.0 (X11; U; Linux "
+						+ "i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)");
+		httpPost.setHeader(
+				"Accept",
+				"text/html,application/xml,"
+						+ "application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5");
+		httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+		httpPost.setHeader("Referer", "http://vozme.com/index.php?lang=en");
+
+		httpPost.setHeader("Host", "vozme.com");
+
+		try {
+			// tmp = new StringEntity(data,"UTF-8");
+
+			tmp = new StringEntity(newdata);
+
+		} catch (UnsupportedEncodingException e) {
+			System.out
+					.println("HTTPHelp : UnsupportedEncodingException : " + e);
+		}
+
+		httpPost.setEntity(tmp);
+
+		try {
+			response = httpClient.execute(httpPost, localContext);
+		} catch (ClientProtocolException e) {
+			System.out.println("HTTPHelp : ClientProtocolException : " + e);
+		} catch (IOException e) {
+			System.out.println("HTTPHelp : IOException : " + e);
+		} catch (Exception e) {
+			System.out.println("HTTPHelp : Exception : " + e.getMessage());
+		}
+		ret = response.getStatusLine().toString();
+		HttpEntity entity = response.getEntity();
+		String responseText = EntityUtils.toString(entity);
+
+		return responseText;
+	}
+
+	public String getVozMeAudioUrl(String response) throws Exception {
+
+		// xu ly reponse string
+		int test = response.lastIndexOf("href");
+		int test2 = response.lastIndexOf("onclick");
+		String test3 = response.substring(test + 6, test2 - 2);
+
+		return test3.replace("\"", "");
+
+	}
 }
-
-
-
