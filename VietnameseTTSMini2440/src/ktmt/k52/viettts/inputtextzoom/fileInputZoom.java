bd0ef72c7f827c10ff32993cca44d48993385131
nguyendung.dce.hut <nguyendung.dce.hut@gmail.com>
2012-03-26 03:07:54 +0000

    Hoàn thiện lại bàn phím để có thể chạy được trên KIT Khi chạy trên KIT vẫn hơi thiếu nút đã viết được tiếng việt bằng cả bàn phím ảo và thật

--- VietnameseTTSMini2440/src/ktmt/k52/viettts/inputtextzoom/fileInputZoom.java
+++ VietnameseTTSMini2440/src/ktmt/k52/viettts/inputtextzoom/fileInputZoom.java
@@ -18,11 +18,17 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,8 +38,8 @@ import android.widget.Toast;
  * @see Activity
  * @author DungNT
  */
-public class fileInputZoom extends Activity implements
-OnKeyboardActionListener, OnKeyListener{
+public class fileInputZoom extends Activity implements OnTouchListener, OnClickListener,
+OnFocusChangeListener,OnKeyListener{
 	/**
 	 * Giao diện để người dùng nhập tiếng việt
 	 * @see EditText
@@ -85,9 +91,23 @@ OnKeyboardActionListener, OnKeyListener{
 	protected static final int REQUEST_CODE = 0;
 
 	EditText currentEditText = null;
-	private static boolean shift = false;
-	private KeyboardView keyboardView;
-	private Keyboard qwertyKeyboard,shiftKeyboard;
+	
+	private Button mBSpace, mBdone, mBack, mBChange, mNum;
+	private RelativeLayout mLayout, mKLayout;
+	private boolean isEdit = false;
+	private String mUpper = "upper", mLower = "lower";
+	private int w, mWindowWidth;
+	private String sL[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+			"k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+			"x", "y", "z", ".", "]", "[", ":", ";", "," };
+	private String cL[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+			"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+			"X", "Y", "Z", ".", "]", "[", ":", ";", "," };
+	private String nS[] = { "!", ")", "'", "#", "3", "$", "%", "&", "8", "*",
+			"?", "/", "+", "-", "9", "0", "1", "4", "@", "5", "7", "(", "2",
+			"\"", "6", "_", ".", "]", "[", "<", ">", "," };
+	private Button mB[] = new Button[32];
+	
 	/**
 	 * hàm tạo activity
 	 * @param savedInstanceState
@@ -95,19 +115,27 @@ OnKeyboardActionListener, OnKeyListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
+		//new add
+		// adjusting key regarding window sizes
+	
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.textinputzoom);
-		//set tieng viet
-		 keyboardView = (KeyboardView) findViewById(R.id.keyboardview);
-		 qwertyKeyboard = new Keyboard(this, R.xml.qwerty);
-		 shiftKeyboard = new Keyboard(this,R.xml.qwertyshift);
-		keyboardView.setKeyboard(qwertyKeyboard);
-		keyboardView.setEnabled(true);
-		keyboardView.setPreviewEnabled(true);
-		keyboardView.setOnKeyListener(this);
-		keyboardView.setOnKeyboardActionListener(this);
+		setKeys();
+		setFrow();
+		setSrow();
+		setTrow();
+		setForow();
+		
 		// init
 		inputzoom = (EditText) findViewById(R.id.InputZoom);
+		inputzoom.setOnTouchListener(this);
+		inputzoom.setOnFocusChangeListener(this);
+		inputzoom.setOnClickListener(this);
+		mLayout = (RelativeLayout) findViewById(R.id.xK1);
+		mKLayout = (RelativeLayout) findViewById(R.id.xKeyBoard);
+		
+		
 		clearInput = (ImageButton) findViewById(R.id.clearInputZoom);
 
 		goBack = (ImageButton) findViewById(R.id.back);
@@ -131,12 +159,7 @@ OnKeyboardActionListener, OnKeyListener{
 				return false;
 			}
 		});
-		inputzoom.setOnClickListener(new View.OnClickListener() {
-
-			public void onClick(View v) {
-				toggleKeyboardVisibility(inputzoom);
-			}
-		});
+		
 		// cac su kien nut an
 		clearInput.setOnClickListener(new OnClickListener() {
 
@@ -271,158 +294,304 @@ OnKeyboardActionListener, OnKeyListener{
 
 	}
 	
-	private void toggleKeyboardVisibility(EditText ei) {
-		KeyboardView keyboardView = (KeyboardView) findViewById(R.id.keyboardview);
-		int visibility = keyboardView.getVisibility();
-		switch (visibility) {
-		case View.VISIBLE:
-			keyboardView.setVisibility(View.GONE);
-			break;
-		case View.GONE:
-		case View.INVISIBLE:
-			keyboardView.setVisibility(View.VISIBLE);
-			currentEditText = ei;
-			break;
-		}
-	}
+	
 	@Override
 	public boolean onKey(View v, int keyCode, KeyEvent event) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
 	@Override
-	public void onKey(int primaryCode, int[] keyCodes) {
-		  HashMap<String, String> keyCodeMap = new HashMap<String, String>();
-		    /*   keyCodeMap.put("1", "1");
-		       keyCodeMap.put("2", "2");
-		       keyCodeMap.put("3", "3");
-		       keyCodeMap.put("4", "4");
-		       keyCodeMap.put("5", "5");
-		       keyCodeMap.put("6", "6");
-		       keyCodeMap.put("7", "7");
-		       keyCodeMap.put("8", "8");
-		       keyCodeMap.put("9", "9");
-		       keyCodeMap.put("0", "0");*/
-		       keyCodeMap.put("11", "q");
-		       keyCodeMap.put("12", "w");
-		       keyCodeMap.put("13", "e");
-		       keyCodeMap.put("14", "r");
-		       keyCodeMap.put("15", "t");
-		       keyCodeMap.put("16", "y");
-		       keyCodeMap.put("17", "u");
-		       keyCodeMap.put("18", "i");
-		       keyCodeMap.put("19", "o");
-		       keyCodeMap.put("10", "p");
-		       keyCodeMap.put("21", "a");
-		       keyCodeMap.put("22", "s");
-		       keyCodeMap.put("23", "d");
-		       keyCodeMap.put("24", "f");
-		       keyCodeMap.put("25", "g");
-		       keyCodeMap.put("26", "h");
-		       keyCodeMap.put("27", "j");
-		       keyCodeMap.put("28", "k");
-		       keyCodeMap.put("29", "l");
-		       keyCodeMap.put("31", "z");
-		       keyCodeMap.put("32", "x");
-		       keyCodeMap.put("33", "c");
-		       keyCodeMap.put("34", "v");
-		       keyCodeMap.put("35", "b");
-		       keyCodeMap.put("36", "n");
-		       keyCodeMap.put("37", "m");
-		       
-		       keyCodeMap.put("111", "Q");
-		       keyCodeMap.put("112", "W");
-		       keyCodeMap.put("113", "E");
-		       keyCodeMap.put("114", "R");
-		       keyCodeMap.put("115", "T");
-		       keyCodeMap.put("116", "Y");
-		       keyCodeMap.put("117", "U");
-		       keyCodeMap.put("118", "I");
-		       keyCodeMap.put("119", "O");
-		       keyCodeMap.put("110", "P");
-		       keyCodeMap.put("121", "A");
-		       keyCodeMap.put("122", "S");
-		       keyCodeMap.put("123", "D");
-		       keyCodeMap.put("124", "F");
-		       keyCodeMap.put("125", "G");
-		       keyCodeMap.put("126", "H");
-		       keyCodeMap.put("127", "J");
-		       keyCodeMap.put("128", "K");
-		       keyCodeMap.put("129", "L");
-		       keyCodeMap.put("131", "Z");
-		       keyCodeMap.put("132", "X");
-		       keyCodeMap.put("133", "C");
-		       keyCodeMap.put("134", "V");
-		       keyCodeMap.put("135", "B");
-		       keyCodeMap.put("136", "N");
-		       keyCodeMap.put("137", "M");
-		       
-		       keyCodeMap.put("-3", "\n");
-		       keyCodeMap.put("-2", ",");
-		       keyCodeMap.put("-1", "!");
-		       keyCodeMap.put("-4", ".");
-		       keyCodeMap.put("-5", " ");
-		       keyCodeMap.put("-6", "?");
-		       
-		       	String c = keyCodeMap.get(String.valueOf(primaryCode));
-	       if(!(c == null)){
-	        currentEditText.append(c);
-	        VietKeyListenner.setKey3(currentEditText);
-	       }
-	       else{
-	        switch(primaryCode){
-	        case -7:
-	         if(currentEditText.getText().toString().length() > 0){	 
-	          currentEditText.setText(currentEditText.getText().toString().substring(0, currentEditText.getText().toString().length() - 1));
-	         }
-	         break;
-	        case 45://shift
-	        	shift = !shift;
-	        	if(shift)
-	        	{
-	        	keyboardView.setKeyboard(shiftKeyboard);	
-	        	}else{
-	        		keyboardView.setKeyboard(qwertyKeyboard);	
-	        	}
-	        	break;
-	        	}
-	        }
+	public void onFocusChange(View v, boolean hasFocus) {
+		if (v == inputzoom && hasFocus == true) {
+			isEdit = true;
+		
+
+		} 
 		
 	}
 	@Override
-	public void onPress(int primaryCode) {
-		// TODO Auto-generated method stub
+	public void onClick(View v) {
+		if (v == mBChange) {
+
+			if (mBChange.getTag().equals(mUpper)) {
+				changeSmallLetters();
+				changeSmallTags();
+			} else if (mBChange.getTag().equals(mLower)) {
+				changeCapitalLetters();
+				changeCapitalTags();
+			}
+
+		} else if (v != mBdone && v != mBack && v != mBChange && v != mNum) {
+			addText(v);
+
+		} else if (v == mBdone) {
+
+			disableKeyboard();
+
+		} else if (v == mBack) {
+			isBack(v);
+		} else if (v == mNum) {
+			String nTag = (String) mNum.getTag();
+			if (nTag.equals("num")) {
+				changeSyNuLetters();
+				changeSyNuTags();
+				mBChange.setVisibility(Button.INVISIBLE);
+
+			}
+			if (nTag.equals("ABC")) {
+				changeCapitalLetters();
+				changeCapitalTags();
+			}
+
+		}
 		
 	}
 	@Override
-	public void onRelease(int primaryCode) {
-		// TODO Auto-generated method stub
-		
+	public boolean onTouch(View v, MotionEvent event) {
+		if (v == inputzoom) {
+			hideDefaultKeyboard();
+			enableKeyboard();
+
+		}
+		return true;
 	}
-	@Override
-	public void onText(CharSequence text) {
-		// TODO Auto-generated method stub
-		
+	private void addText(View v) {
+		if (isEdit == true) {
+			String b = "";
+			b = (String) v.getTag();
+			if (b != null) {
+				// adding text in Edittext
+				inputzoom.append(b);
+				  VietKeyListenner.setKey3(inputzoom);
+
+			}
+		}
+		}
+	
+	private void isBack(View v) {
+		if (isEdit == true) {
+			CharSequence cc = inputzoom.getText();
+			if (cc != null && cc.length() > 0) {
+				{
+					inputzoom.setText("");
+					inputzoom.append(cc.subSequence(0, cc.length() - 1));
+					
+				}
+
+			}
+		}
 	}
-	@Override
-	public void swipeDown() {
-		// TODO Auto-generated method stub
-		
+	
+	private void changeSmallLetters() {
+		mBChange.setVisibility(Button.VISIBLE);
+		for (int i = 0; i < sL.length; i++)
+			mB[i].setText(sL[i]);
+		mNum.setTag("123");
 	}
-	@Override
-	public void swipeLeft() {
-		// TODO Auto-generated method stub
-		
+	private void changeSmallTags() {
+		for (int i = 0; i < sL.length; i++)
+			mB[i].setTag(sL[i]);
+		mBChange.setTag("lower");
+		mNum.setTag("num");
 	}
-	@Override
-	public void swipeRight() {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void swipeUp() {
-		// TODO Auto-generated method stub
-		
+	private void changeCapitalLetters() {
+		mBChange.setVisibility(Button.VISIBLE);
+		for (int i = 0; i < cL.length; i++)
+			mB[i].setText(cL[i]);
+		mBChange.setTag("upper");
+		mNum.setText("123");
+
 	}
 
-}
+	private void changeCapitalTags() {
+		for (int i = 0; i < cL.length; i++)
+			mB[i].setTag(cL[i]);
+		mNum.setTag("num");
+
+	}
+
+	private void changeSyNuLetters() {
+
+		for (int i = 0; i < nS.length; i++)
+			mB[i].setText(nS[i]);
+		mNum.setText("ABC");
+	}
+
+	private void changeSyNuTags() {
+		for (int i = 0; i < nS.length; i++)
+			mB[i].setTag(nS[i]);
+		mNum.setTag("ABC");
+	}
+
+	// enabling customized keyboard
+	private void enableKeyboard() {
+
+		mLayout.setVisibility(RelativeLayout.VISIBLE);
+		mKLayout.setVisibility(RelativeLayout.VISIBLE);
+
+	}
+
+	// Disable customized keyboard
+	private void disableKeyboard() {
+		mLayout.setVisibility(RelativeLayout.INVISIBLE);
+		mKLayout.setVisibility(RelativeLayout.INVISIBLE);
+
+	}
+
+	private void hideDefaultKeyboard() {
+		getWindow().setSoftInputMode(
+				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+	}
+
+	private void setFrow() {
+		w = (mWindowWidth / 13);
+		w = w - 15;
+		mB[16].setWidth(w);
+		mB[22].setWidth(w + 3);
+		mB[4].setWidth(w);
+		mB[17].setWidth(w);
+		mB[19].setWidth(w);
+		mB[24].setWidth(w);
+		mB[20].setWidth(w);
+		mB[8].setWidth(w);
+		mB[14].setWidth(w);
+		mB[15].setWidth(w);
+		mB[16].setHeight(50);
+		mB[22].setHeight(50);
+		mB[4].setHeight(50);
+		mB[17].setHeight(50);
+		mB[19].setHeight(50);
+		mB[24].setHeight(50);
+		mB[20].setHeight(50);
+		mB[8].setHeight(50);
+		mB[14].setHeight(50);
+		mB[15].setHeight(50);
+
+	}
+
+	private void setSrow() {
+		w = (mWindowWidth / 10);
+		mB[0].setWidth(w);
+		mB[18].setWidth(w);
+		mB[3].setWidth(w);
+		mB[5].setWidth(w);
+		mB[6].setWidth(w);
+		mB[7].setWidth(w);
+		mB[26].setWidth(w);
+		mB[9].setWidth(w);
+		mB[10].setWidth(w);
+		mB[11].setWidth(w);
+		mB[26].setWidth(w);
+
+		mB[0].setHeight(50);
+		mB[18].setHeight(50);
+		mB[3].setHeight(50);
+		mB[5].setHeight(50);
+		mB[6].setHeight(50);
+		mB[7].setHeight(50);
+		mB[9].setHeight(50);
+		mB[10].setHeight(50);
+		mB[11].setHeight(50);
+		mB[26].setHeight(50);
+	}
+
+	private void setTrow() {
+		w = (mWindowWidth / 12);
+		mB[25].setWidth(w);
+		mB[23].setWidth(w);
+		mB[2].setWidth(w);
+		mB[21].setWidth(w);
+		mB[1].setWidth(w);
+		mB[13].setWidth(w);
+		mB[12].setWidth(w);
+		mB[27].setWidth(w);
+		mB[28].setWidth(w);
+		mBack.setWidth(w);
+
+		mB[25].setHeight(50);
+		mB[23].setHeight(50);
+		mB[2].setHeight(50);
+		mB[21].setHeight(50);
+		mB[1].setHeight(50);
+		mB[13].setHeight(50);
+		mB[12].setHeight(50);
+		mB[27].setHeight(50);
+		mB[28].setHeight(50);
+		mBack.setHeight(50);
+
+	}
+
+	private void setForow() {
+		w = (mWindowWidth / 10);
+		mBSpace.setWidth(w * 4);
+		mBSpace.setHeight(50);
+		mB[29].setWidth(w);
+		mB[29].setHeight(50);
+
+		mB[30].setWidth(w);
+		mB[30].setHeight(50);
+
+		mB[31].setHeight(50);
+		mB[31].setWidth(w);
+		mBdone.setWidth(w + (w / 1));
+		mBdone.setHeight(50);
+
+	}
+
+	private void setKeys() {
+		mWindowWidth = getWindowManager().getDefaultDisplay().getWidth(); // getting
+		// window
+		// height
+		// getting ids from xml files
+		mB[0] = (Button) findViewById(R.id.xA);
+		mB[1] = (Button) findViewById(R.id.xB);
+		mB[2] = (Button) findViewById(R.id.xC);
+		mB[3] = (Button) findViewById(R.id.xD);
+		mB[4] = (Button) findViewById(R.id.xE);
+		mB[5] = (Button) findViewById(R.id.xF);
+		mB[6] = (Button) findViewById(R.id.xG);
+		mB[7] = (Button) findViewById(R.id.xH);
+		mB[8] = (Button) findViewById(R.id.xI);
+		mB[9] = (Button) findViewById(R.id.xJ);
+		mB[10] = (Button) findViewById(R.id.xK);
+		mB[11] = (Button) findViewById(R.id.xL);
+		mB[12] = (Button) findViewById(R.id.xM);
+		mB[13] = (Button) findViewById(R.id.xN);
+		mB[14] = (Button) findViewById(R.id.xO);
+		mB[15] = (Button) findViewById(R.id.xP);
+		mB[16] = (Button) findViewById(R.id.xQ);
+		mB[17] = (Button) findViewById(R.id.xR);
+		mB[18] = (Button) findViewById(R.id.xS);
+		mB[19] = (Button) findViewById(R.id.xT);
+		mB[20] = (Button) findViewById(R.id.xU);
+		mB[21] = (Button) findViewById(R.id.xV);
+		mB[22] = (Button) findViewById(R.id.xW);
+		mB[23] = (Button) findViewById(R.id.xX);
+		mB[24] = (Button) findViewById(R.id.xY);
+		mB[25] = (Button) findViewById(R.id.xZ);
+		mB[26] = (Button) findViewById(R.id.xS1);
+		mB[27] = (Button) findViewById(R.id.xS2);
+		mB[28] = (Button) findViewById(R.id.xS3);
+		mB[29] = (Button) findViewById(R.id.xS4);
+		mB[30] = (Button) findViewById(R.id.xS5);
+		mB[31] = (Button) findViewById(R.id.xS6);
+		mBSpace = (Button) findViewById(R.id.xSpace);
+		mBdone = (Button) findViewById(R.id.xDone);
+		mBChange = (Button) findViewById(R.id.xChange);
+		mBack = (Button) findViewById(R.id.xBack);
+		mNum = (Button) findViewById(R.id.xNum);
+		for (int i = 0; i < mB.length; i++)
+			mB[i].setOnClickListener(this);
+		mBSpace.setOnClickListener(this);
+		mBdone.setOnClickListener(this);
+		mBack.setOnClickListener(this);
+		mBChange.setOnClickListener(this);
+		mNum.setOnClickListener(this);
+
+	}
+
+	}
+
+
